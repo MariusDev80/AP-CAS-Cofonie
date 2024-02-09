f94f822c5f8fc48fd05af49dcb5b0929e6883c64
Marius Dudouet <marius.dudouet@outlook.fr>
2024-02-09 15:24:38 +0100

    changements

--- menu.php
+++ menu.php
@@ -1,36 +1,60 @@
-            <div class="dropdow col">
-                <button class="btn bg-transparent dropdown-toogle" type="button" id="menu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Menu 1
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="menuVehicule">
-		            <li><a class="dropdown-item" href='index.php?vue=vehicule&action=ajouter'>Ajouter un Vehicule</a></li>
-		            <li><a class="dropdown-item" href='index.php?vue=vehicule&action=visualiser'>Visualiser un Vehicule</a></li>
-	            </ul>
-            </div>
-            <div class="dropdown col">
-                <button class="btn bg-transparent dropdown-toogle" type="button" id="menu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-		            Menu 2 
-		            <span class="caret"></span>
-                    
-                    </button>
-	                
-	            
-                <ul class="dropdown-menu" aria-labelledby="menuAgence">
-		            <li><a class="dropdown-item" href = 'index.php?vue=amendement&action=ajouter'>Ajouter les agences</a></li>
-		            <li><a class="dropdown-item" href = 'index.php?vue=amendement&action=visualiser'>Visualiser les agences</a></li>
-	            </ul>
-            </div>
-        </div>
-        <div class="container-fluid">
-            <div class ="infosComplementaires">
-                <br><h3>Informations</h3>
-                ici se trouvent des informations complémentaires
-                <br>
-                ...
-            </div>
-            <div class="affichage">
-                Affichage global
-            </div>
-
-        </div>
+            <h3>Menu Global</h3>
+            <ul class="nav-links">
+              <li>
+                <a href="#">
+                  Articles
+                </a>
+                <ul class="sub-menu">
+                  <li><a href="#">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+              <li>
+                  <a href="#">
+                    Amendements
+                  </a>
+                <ul class="sub-menu">
+                  <li><a href="index.php?vue=amendement&action=visualiser">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+              <li>
+                  <a href="#">
+                    Institutions
+                  </a>
+                <ul class="sub-menu">
+                  <li><a href="#">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">
+                  Types Institutions
+                </a>
+                <ul class="sub-menu">
+                  <li><a href="#">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">
+                  Organes
+                </a>
+                <ul class="sub-menu">
+                  <li><a href="#">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">
+                  Rôles
+                </a>
+                <ul class="sub-menu">
+                  <li><a href="#">Ajouter</a></li>
+                  <li><a href="#">Visualiser</a></li>
+                </ul>
+              </li>
+        </ul>
+      </div>
+    </div>
+    <div class="page">
