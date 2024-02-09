0da7fc987f7cfd6c2b50d33cf4d45a024cec2a3b
Marius Dudouet <marius.dudouet@outlook.fr>
2024-02-09 23:16:29 +0100

    test

--- metierAmendement.php
+++ metierAmendement.php
@@ -7,16 +7,16 @@ class metierAmendement {
 
     public function __get($attribut){
         switch ($attribut){
-            case 'idAmendement': return $this->id; break;
-            case 'dateAmendement':return $this->dateTime; break;
+            case 'idAmendement': return $this->idAmendement; break;
+            case 'dateAmendement':return $this->dateAmendement; break;
             case 'texteAmendement':return $this->texteAmendement; break;
         }
     }
 
     public function __set($attribut,$valeur){
         switch($attribut){
-            case 'idAmendement': $this->id = $valeur; break;
-            case '$dateAmendement': $this->dateTime = $valeur; break;
+            case 'idAmendement': $this->idAmendement = $valeur; break;
+            case '$dateAmendement': $this->dateAmendement = $valeur; break;
             case 'texteAmendement': $this->texteAmendement = $valeur; break;
         }
     }
